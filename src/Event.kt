95a8509e13223e7a27237b8424dba470805d29ea
Sebastian Tamayo <sebastamayo37@gmail.com>
2024-10-15 19:44:36 -0500

    Practice: Classes and Collections

---
+++ src/Event.kt
@@ -0,0 +1,14 @@
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+
+val Event.durationOfEvent: String
+    get() = if (this.durationInMinutes < 60) {
+        "short"
+    } else {
+        "long"
+    }
+
